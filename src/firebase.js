248279a08bdffcc31318bbfc24499c4ed0bbb204
kavya <kavya.actimize@gmail.com>
2026-05-11 16:04:01 +0530

    update

--- src/firebase.js
+++ src/firebase.js
@@ -8,6 +8,7 @@ const firebaseConfig = {
   storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
   messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
   appId: process.env.REACT_APP_APP_ID,
+  dataBaseUrl:process.env.REACT_APP_DATABASE_URL
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
